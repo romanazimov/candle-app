2f20ab9bb4510593d66206f0b4ea3f57d76259fa
Roman <36523168+romanazimov@users.noreply.github.com>
2023-09-11 15:16:36 -0400

    Hero section finished

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,9 +6,10 @@ function Hero() {
         <section id='Hero'>
             <img className="hero-img" src={bg_image} alt="bg image" />
             <div className='center-container'>
-                <div>
-                    <p>The nature candle</p>
-                    <p>All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments</p>
+                <div className='text-cont'>
+                    <p className='title-icon'>🌱</p>
+                    <p className='title'>The nature candle</p>
+                    <p className='description'>All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments</p>
                 </div>
                 <button>Discover our collection</button>
             </div>
