21a2ee5457e382980baf95cc06c1b602a1e12213
Roman <36523168+romanazimov@users.noreply.github.com>
2023-09-14 10:56:43 -0400

    Finished responsive Benefits section

--- src/sections/Benefits.tsx
+++ src/sections/Benefits.tsx
@@ -6,19 +6,63 @@ function Benefits() {
     return (
         <section id='Benefits'>
             <div className='container'>
-                <div className='cont-left'>
+                <div className='upper-header'>
                     <p className='title'>Clean and fragrant soy wax</p>
                     <p className='description'>Made for your home and for your wellness</p>
-                    <ul>
-                        <li><AiOutlineCheckCircle /><span className='li-tag'>Eco-sustainable:</span> All recyclable materials, 0% CO2 emissions.</li>
-                        <li><AiOutlineCheckCircle /><span className='li-tag'>Hypoallergenic:</span> 100% natural, human friendly ingredients.</li>
-                        <li><AiOutlineCheckCircle /><span className='li-tag'>Handmade:</span> All candles are craftly made with love.</li>
-                        <li><AiOutlineCheckCircle /><span className='li-tag'>Long lasting:</span> No more waste. Created to last!</li>
-                    </ul>
-                    <button>Learn more</button>
                 </div>
-                <div className='cont-right'>
-                    <img className="benefits-img" src={benefits_image} alt="benefits image" />
+                <div className='split-container'>
+                    <div className='cont-left'>
+                        <div className='left-header'>
+                            <p className='title'>Clean and fragrant soy wax</p>
+                            <p className='description'>Made for your home and for your wellness</p>
+                        </div>
+                        <ul>
+                            <li>
+                                <div className='li-cont'>
+                                    <div>
+                                        <AiOutlineCheckCircle className='bullet' />
+                                    </div>
+                                    <div>
+                                        <b>Eco-sustainable:</b> All recyclable materials, 0% CO2 emissions.
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='li-cont'>
+                                    <div>
+                                        <AiOutlineCheckCircle className='bullet' />
+                                    </div>
+                                    <div>
+                                        <b>Hypoallergenic:</b> 100% natural, human friendly ingredients.
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='li-cont'>
+                                    <div>
+                                        <AiOutlineCheckCircle className='bullet' />
+                                    </div>
+                                    <div>
+                                        <b>Handmade:</b> All candles are craftly made with love.
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='li-cont'>
+                                    <div>
+                                        <AiOutlineCheckCircle className='bullet' />
+                                    </div>
+                                    <div>
+                                        <b>Long lasting:</b> No more waste. Created to last!
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <button>Learn more</button>
+                    </div>
+                    <div className='cont-right'>
+                        <img className="benefits-img" src={benefits_image} alt="benefits image" />
+                    </div>
                 </div>
             </div>
         </section>
